0cf39dc0741f48a41efab6c76039907482b47191
shanart <shanin.x@gmail.com>
2020-07-19 21:25:50 +0300

    updates

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//
 var _carousel = $('[data-c-area="circle-carousel"]');
 
 var _btn_next = $('[data-c-action="next"]');
@@ -13,7 +12,35 @@ _carousel.find('.__circle_item').clone().appendTo(_carousel); // reverse origina
 
 var _original_items = $($('[data-c-area="circle-carousel"] .__circle_item').get().reverse());
 
-_carousel.append(_original_items);
+_carousel.append(_original_items); // get middle
+
+
+function make_middle() {
+  var __i = _carousel.find('.__circle_item');
+
+  __i.each(function () {
+    $(this).removeClass('middle middle_prev_1 middle_prev_2 middle_next_1 middle_next_2');
+  });
+
+  var __middle = __i[__i.length / 2 - 1];
+  $(__middle).addClass('middle'); // get prev 2 items
+  // __ get middle - 1
+
+  var __m_prev_1 = __i[__i.length / 2 - 2];
+  $(__m_prev_1).addClass('middle_prev_1'); // __ get middle - 2
+
+  var __m_prev_2 = __i[__i.length / 2 - 3];
+  $(__m_prev_2).addClass('middle_prev_2'); // get next 2 items
+  // __ get middle + 1
+
+  var __m_next_1 = __i[__i.length / 2];
+  $(__m_next_1).addClass('middle_next_1'); // __ get middle + 2
+
+  var __m_next_2 = __i[__i.length / 2 + 1];
+  $(__m_next_2).addClass('middle_next_2');
+}
+
+make_middle();
 
 _carousel.on('circle_slider:next', function () {
   // delete last item
@@ -21,6 +48,8 @@ _carousel.on('circle_slider:next', function () {
 
 
   _last.prependTo(_carousel);
+
+  make_middle();
 });
 
 _carousel.on('circle_slider:prev', function () {
@@ -29,6 +58,8 @@ _carousel.on('circle_slider:prev', function () {
 
 
   _first.appendTo(_carousel);
+
+  make_middle();
 });
 
 _btn_next.on('click', function () {
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-
-//
-
 const _carousel = $('[data-c-area="circle-carousel"]');
 const _btn_next = $('[data-c-action="next"]');
 const _btn_prev = $('[data-c-action="prev"]');
@@ -12,12 +9,44 @@ _carousel.find('.__circle_item').clone().appendTo(_carousel);
 const _original_items = $($('[data-c-area="circle-carousel"] .__circle_item').get().reverse());
 _carousel.append(_original_items);
 
+// get middle
+function make_middle() {
+    const __i =_carousel.find('.__circle_item');
+    __i.each(function(){
+        $(this).removeClass('middle middle_prev_1 middle_prev_2 middle_next_1 middle_next_2');
+    });
+    let __middle = __i[__i.length/2 - 1];
+    $(__middle).addClass('middle');
+
+    // get prev 2 items
+    // __ get middle - 1
+    const __m_prev_1 = __i[__i.length/2 - 2];
+    $(__m_prev_1).addClass('middle_prev_1');
+
+    // __ get middle - 2
+    const __m_prev_2 = __i[__i.length/2 - 3];
+    $(__m_prev_2).addClass('middle_prev_2');
+
+    // get next 2 items
+    // __ get middle + 1
+    const __m_next_1 = __i[__i.length/2];
+    $(__m_next_1).addClass('middle_next_1');
+
+    // __ get middle + 2
+    const __m_next_2 = __i[__i.length/2 + 1];
+    $(__m_next_2).addClass('middle_next_2');
+}
+
+make_middle();
+
+
 _carousel.on('circle_slider:next', function(){
     // delete last item
     const _last = $('[data-c-area="circle-carousel"] .__circle_item:last-child');
     
     // append to start of _carousel()
     _last.prependTo(_carousel);
+    make_middle();
 
 });
 
@@ -26,6 +55,9 @@ _carousel.on('circle_slider:prev', function(){
     const _first = $('[data-c-area="circle-carousel"] .__circle_item:first-child');
     // append to end of _carousel()
     _first.appendTo(_carousel);
+
+    make_middle();
+
 });
 
 
